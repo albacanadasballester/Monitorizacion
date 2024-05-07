07bc395a77dab5ae05e0dabc07d62681d2f82d1a
albacanadasballester <alba.canadas@axians.es>
2024-05-07 14:21:20 +0200

    modificacion script

--- cargar_logs.py
+++ cargar_logs.py
@@ -1,10 +1,9 @@
 import socket
-UDP_IP = "10.88.0.4"
+UDP_IP = "192.168.104.41"
 UDP_PORT = 5013
 
 # Abre el archivo en modo lectura
 with open('muestra.log', 'r',encoding='iso-8859-1') as archivo:
-    # Lee las primeras 10 líneas del archivo
     lineas = archivo.readlines()
 
 sock = socket.socket(socket.AF_INET, # Internet
@@ -13,7 +12,6 @@ sock = socket.socket(socket.AF_INET, # Internet
 
 # Itera sobre cada línea y envíala a OpenSearch
 for linea in lineas:
-    print(f'voy a enviar esta linea {linea}')
+    print(f'voy a insertar la linea {linea}')
     mensaje = linea.encode() #cambiar la codificacion
     sock.sendto(mensaje, (UDP_IP, UDP_PORT))
-
